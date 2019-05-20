3ac3ca8b35f8dc8500d3a0cfbf686ba79bc0a0ea
Tinshu Sasi <thetinshusasi@gmail.com>
2019-05-20 23:42:23 +0530

    Deleting

--- BondIssuanceHackFest.WebAPI/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace BondIssuanceHackFest.WebAPI.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
--- BondIssuanceHackFest.WebAPI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace BondIssuanceHackFest.WebAPI.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
